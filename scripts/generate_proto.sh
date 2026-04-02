3965fb445679d414fef9e41f0eb98bee152775eb
cfr <447483932@qq.com>
2026-04-02 17:21:09 +0800

    代码整理

--- scripts/generate_proto.sh
+++ scripts/generate_proto.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # 生成gRPC代码
-protoc --go_out=. --go-grpc_out=. pkg/proto/auth.proto
-protoc --go_out=. --go-grpc_out=. pkg/proto/game.proto
-protoc --go_out=. --go-grpc_out=. pkg/proto/bill.proto
-protoc --go_out=. --go-grpc_out=. pkg/proto/chat.proto
-protoc --go_out=. --go-grpc_out=. pkg/proto/gateway.proto
+protoc --go_out=. --go-grpc_out=. proto/auth.proto
+protoc --go_out=. --go-grpc_out=. proto/game.proto
+protoc --go_out=. --go-grpc_out=. proto/bill.proto
+protoc --go_out=. --go-grpc_out=. proto/chat.proto
+protoc --go_out=. --go-grpc_out=. proto/gateway.proto
 
 echo "Proto files generated successfully!"
